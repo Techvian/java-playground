bde922dbfeda50ba94a2cc72c36bd4835bb67658
Techvian <ayushks.work07@gmail.com>
2026-05-02 13:34:16 +0530

    add-image and modified

--- patterns-code/square.java
+++ patterns-code/square.java
@@ -1,5 +1,14 @@
 public class square {
     public static void main(String[] args) {
-        
+        final int n = 4;
+        final int m = 5;
+        //outer loop
+        for (int i=1; i<=n; i++) {
+            //inner loop
+            for (int j=1; j<=m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
