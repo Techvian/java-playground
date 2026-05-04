216ccabb4265a6bdff720e5cc9e810f06dbfdd56
Techvian <ayushks.work07@gmail.com>
2026-05-05 03:18:01 +0530

    learning

---
+++ patterns-code/pyr_reverse_right.java
@@ -0,0 +1,13 @@
+public class pyr_reverse_right {
+    public static void main(String[] args) {
+        final int n = 5;
+        //outer loop
+        for (int i=n; i>=1; i--) {
+            //inner loop
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
