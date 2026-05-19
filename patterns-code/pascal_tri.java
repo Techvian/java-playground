7bdd91a440e771a48f6063fa483dea883ee36192
Techvian <ayushks.work07@gmail.com>
2026-05-20 01:07:13 +0530

    add-new-pattern

---
+++ patterns-code/pascal_tri.java
@@ -0,0 +1,20 @@
+public class pascal_tri {
+    public static void main(String[] args) {
+        final int n=4;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<n-i; j++) {
+                System.out.print(" ");
+            }
+            //star
+            int m=1;
+            for (int j=1; j<=i; j++) {
+                System.out.print(m+"  ");
+
+                if (j<i) {
+                    m = m*(i-j)/j+1;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
