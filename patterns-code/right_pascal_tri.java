db7924d981a72715faed2ae0b90dde6623f8ab7e
Techvian <ayushks.work07@gmail.com>
2026-07-13 11:12:17 +0530

    start DSA

--- patterns-code/right_pascal_tri.java
+++ patterns-code/right_pascal_tri.java
@@ -1,14 +1,6 @@
 public class right_pascal_tri {
     public static void main(String[] args) {
         final int n=4;
-        for (int i=1; i<=n; i++) {
-            for (int j=n; j<=i-1; j++) {
-                System.out.print(" ");
-            }
-            for (int j=1; j<=n; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        
     }
 }
